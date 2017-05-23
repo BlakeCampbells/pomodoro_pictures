36dd45a9b4a646d724b7123c7d5cff7cc644c48e
Blake Campbell <blakecampbell.dev@gmail.com>
2017-05-22 23:16:50 -0700

    Got initial timer working

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,38 +1,64 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="timer"></div>
   </div>
 </template>
 
 <script>
+var ProgressBar = require('progressbar.js')
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted: function () {
+    var bar = new ProgressBar.Circle('#timer', {
+      color: '#aaa',
+      // This has to be the same size as the maximum width to
+      // prevent clipping
+      strokeWidth: 4,
+      trailWidth: 1,
+      easing: 'linear',
+      duration: (60000 * 25),
+      text: {
+        autoStyleContainer: false
+      },
+      from: { color: '#aaa', width: 1 },
+      to: { color: '#333', width: 4 },
+      // Set default step function for all animate calls
+      step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color)
+        circle.path.setAttribute('stroke-width', state.width)
+
+        var secondsPassed = Math.round((circle.value() * 14.4) * 100)
+        var secondsLeft = 1500 - secondsPassed
+        var minutes = Math.floor(secondsLeft / 60)
+        var seconds = secondsLeft - minutes * 60
+        if (secondsPassed === 0) {
+          circle.setText('')
+        } else {
+          circle.setText(minutes + ':' + seconds)
+        }
+      }
+    })
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif'
+    bar.text.style.fontSize = '2rem'
+    bar.animate(1.0)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#timer {
+  margin: 20px;
+  width: 200px;
+  height: 200px;
+  position: relative;
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -45,9 +71,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
