ac7cea78a598829b62598671a012e6576af587a6
Blake Campbell <blakecampbell.dev@gmail.com>
2017-05-28 23:57:30 -0700

    Updating to use variable times and adding buttons for future development

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,6 +2,17 @@
   <div class="hello row">
     <div class="col m4"></div>
     <div class="col m4">
+      <span v-if="showPlay">
+        <a v-on:click="start()" class="btn-floating btn-large waves-effect waves-light red">
+          <i class="material-icons">play_arrow</i>
+        </a>
+      </span>
+      <span v-if="showStop">
+        <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>
+      </span>
+      <span v-if="showReplay">
+        <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>
+      </span>
       <div id="timer"></div>
     </div>
     <div class="col m4"></div>
@@ -13,42 +24,59 @@ var ProgressBar = require('progressbar.js')
 export default {
   name: 'hello',
   data () {
-    return {}
+    return {
+      showPlay: true,
+      showStop: false,
+      showReplay: false
+    }
+  },
+  methods: {
+    start: function () {
+      this.showPlay = false
+      var durationMinutes = 25
+      var bar = new ProgressBar.Circle('#timer', {
+        color: '#ffffff',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 4,
+        trailWidth: 1,
+        easing: 'linear',
+        duration: (60000 * durationMinutes),
+        text: {
+          autoStyleContainer: false
+        },
+        from: { color: '#1ada5b', width: 1 },
+        to: { color: '#000', width: 4 },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+          circle.path.setAttribute('stroke', state.color)
+          circle.path.setAttribute('stroke-width', state.width)
+          // Percentage of progress on circle
+          // var secondsPassed = Math.round((circle.value() * 14.4) * 100)
+          //         var secondsPassed = Math.round((circle._progressPath._opts.duration * circle.value()) / 10000)
+          var secondsPassed = 0
+          var secondsLeft = 0
+          var minutes = 0
+          var seconds = 0
+          if (circle && circle._progressPath) {
+            secondsPassed = Math.round((circle._progressPath._opts.duration * circle.value()) / 1000)
+            secondsLeft = (durationMinutes * 60) - secondsPassed
+            minutes = Math.floor(secondsLeft / 60)
+            seconds = secondsLeft - (minutes * 60)
+          }
+          if (secondsPassed === 0) {
+            circle.setText('')
+          } else {
+            circle.setText(('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2))
+          }
+        }
+      })
+      bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif'
+      bar.text.style.fontSize = '2rem'
+      bar.animate(1.0)
+    }
   },
   mounted: function () {
-    var durationMinutes = 25
-    var bar = new ProgressBar.Circle('#timer', {
-      color: '#ffffff',
-      // This has to be the same size as the maximum width to
-      // prevent clipping
-      strokeWidth: 4,
-      trailWidth: 1,
-      easing: 'linear',
-      duration: (60000 * durationMinutes),
-      text: {
-        autoStyleContainer: false
-      },
-      from: { color: '#1ada5b', width: 1 },
-      to: { color: '#000', width: 4 },
-      // Set default step function for all animate calls
-      step: function (state, circle) {
-        circle.path.setAttribute('stroke', state.color)
-        circle.path.setAttribute('stroke-width', state.width)
-
-        var secondsPassed = Math.round((circle.value() * 14.4) * 100)
-        var secondsLeft = (durationMinutes * 60) - secondsPassed
-        var minutes = Math.floor(secondsLeft / 60)
-        var seconds = secondsLeft - (minutes * 60)
-        if (secondsPassed === 0) {
-          circle.setText('')
-        } else {
-          circle.setText(minutes + ':' + seconds)
-        }
-      }
-    })
-    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif'
-    bar.text.style.fontSize = '2rem'
-    bar.animate(1.0)
   }
 }
 </script>
