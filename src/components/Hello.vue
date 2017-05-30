fc5c8d294fafe4ad67b88bf1caa0c82412d13304
Blake Campbell <blakecampbell.dev@gmail.com>
2017-05-29 21:48:05 -0700

    Adding additional functionality to the chart, start, pause, play, replay

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,37 @@
 <template>
-  <div class="hello row">
-    <div class="col m4"></div>
-    <div class="col m4">
-      <span v-if="showPlay">
-        <a v-on:click="start()" class="btn-floating btn-large waves-effect waves-light red">
-          <i class="material-icons">play_arrow</i>
-        </a>
-      </span>
-      <span v-if="showStop">
-        <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>
-      </span>
-      <span v-if="showReplay">
-        <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">play_arrow</i></a>
-      </span>
-      <div id="timer"></div>
+  <div class="hello">
+    <div class="row">
+      <div class="col m8"></div>
+      <div class="col m4">
+        <span v-if="show.start">
+          <a v-on:click="start()" class="btn-floating btn-large waves-effect waves-light blue lighten-1">
+            <i class="material-icons">play_arrow</i>
+          </a>
+        </span>
+        <span v-if="show.play">
+          <a v-on:click="play()" class="btn-floating btn-large waves-effect waves-light blue lighten-1">
+            <i class="material-icons">play_arrow</i>
+          </a>
+        </span>
+        <span v-if="show.stop">
+          <a v-on:click="stop()" class="btn-floating btn-large waves-effect waves-light blue lighten-1">
+            <i class="material-icons">stop</i>
+          </a>
+        </span>
+        <span v-if="show.replay">
+          <a v-on:click="reset()" class="btn-floating btn-large waves-effect waves-light blue lighten-3">
+            <i class="material-icons">loop</i>
+          </a>
+        </span>
+      </div>
     </div>
-    <div class="col m4"></div>
+    <div class="row">
+      <div class="col m4"></div>
+      <div class="col m4">
+        <div id="timer"></div>
+      </div>
+      <div class="col m4"></div>
+    </div>
   </div>
 </template>
 
@@ -25,23 +41,30 @@ export default {
   name: 'hello',
   data () {
     return {
-      showPlay: true,
-      showStop: false,
-      showReplay: false
+      show: {
+        start: true,
+        stop: false,
+        play: false,
+        replay: false
+      },
+      durationMinutes: 25,
+      circleChart: {}
     }
   },
   methods: {
     start: function () {
-      this.showPlay = false
-      var durationMinutes = 25
-      var bar = new ProgressBar.Circle('#timer', {
+      var self = this
+      this.show.start = false
+      this.show.stop = true
+      this.show.replay = true
+      this.circleChart = new ProgressBar.Circle('#timer', {
         color: '#ffffff',
         // This has to be the same size as the maximum width to
         // prevent clipping
         strokeWidth: 4,
         trailWidth: 1,
         easing: 'linear',
-        duration: (60000 * durationMinutes),
+        duration: (60000 * self.durationMinutes),
         text: {
           autoStyleContainer: false
         },
@@ -52,15 +75,13 @@ export default {
           circle.path.setAttribute('stroke', state.color)
           circle.path.setAttribute('stroke-width', state.width)
           // Percentage of progress on circle
-          // var secondsPassed = Math.round((circle.value() * 14.4) * 100)
-          //         var secondsPassed = Math.round((circle._progressPath._opts.duration * circle.value()) / 10000)
           var secondsPassed = 0
           var secondsLeft = 0
           var minutes = 0
           var seconds = 0
           if (circle && circle._progressPath) {
             secondsPassed = Math.round((circle._progressPath._opts.duration * circle.value()) / 1000)
-            secondsLeft = (durationMinutes * 60) - secondsPassed
+            secondsLeft = (self.durationMinutes * 60) - secondsPassed
             minutes = Math.floor(secondsLeft / 60)
             seconds = secondsLeft - (minutes * 60)
           }
@@ -71,9 +92,28 @@ export default {
           }
         }
       })
-      bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif'
-      bar.text.style.fontSize = '2rem'
-      bar.animate(1.0)
+      this.circleChart.text.style.fontFamily = '"Raleway", Helvetica, sans-serif'
+      this.circleChart.text.style.fontSize = '2rem'
+      this.circleChart.animate(1.0)
+    },
+    stop: function () {
+      var self = this
+      self.show.play = true
+      self.show.stop = false
+      self.circleChart.stop()
+    },
+    play: function () {
+      var self = this
+      self.show.play = false
+      self.show.stop = true
+      self.circleChart.animate(1.0)
+    },
+    reset: function () {
+      var self = this
+      self.show.stop = true
+      self.show.play = false
+      self.circleChart.destroy()
+      self.start()
     }
   },
   mounted: function () {
